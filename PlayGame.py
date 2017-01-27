7562e108fcc358567c09f7a08052c680acf9bc19
Neil Gahart <ngahart1@jh.edu>
2017-01-27 00:19:12 -0500

    Can figure out how many rows user wants, then prints appropriate start board

---
+++ PlayGame.py
@@ -0,0 +1,59 @@
+import sys
+
+"""
+A Python game of Jump-All-But-One, the game
+found at Cracker Barrel Restaurants!
+
+The encoding of the holes is as follows:
+
+                1
+              2   3
+            4   5   6
+           7  8   9   10
+        11  12  13  14  15
+"""
+
+"""
+vec should be a 15-length boolean vector,
+to enable printing in the standard formation.
+true indicates there is a peg in the hole,
+false indicates the hole is empty.
+"""
+def print_board(vec, num_rows):
+
+    for i in vec:
+        if i != 'x' and i != 'o':
+            print('Illegal Game Vector')
+            exit()
+
+    num_printed = 0
+    rows_printed= 0
+    while num_printed < len(vec):
+        print(''.ljust(num_rows - rows_printed - 1), end='')
+        for i in list(range(num_printed, num_printed + rows_printed + 1)):
+            print(vec[i], end = ' ')
+            num_printed += 1
+        print('')
+        rows_printed += 1
+
+
+def getNumRows():
+    try:
+        num_rows = int(input('Enter number of rows: '))
+    except ValueError:
+        print('Must Enter Integer')
+        num_rows = getNumRows()
+    return num_rows
+
+
+def main(num_rows = 5):
+    num_holes = lambda x: int(x*(x+1)/2)
+    num_rows = getNumRows()
+    board = ['o'] * num_holes(num_rows)
+    print_board(board, num_rows)
+
+if __name__ == "__main__":
+    main()
+
+
+
